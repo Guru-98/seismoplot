15154ca371d02b3151514951513ca4e209e4729b
Guru Prasath G <theemailidofthemaster@gmail.com>
2017-04-02 02:10:41 +0530

    isoLines final

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 var bounds;
 var circle;
 var grad = ["rgb(255, 255, 255)", "rgb(191, 204, 255)", "rgb(160, 230, 255)", "rgb(128, 255, 255)", "rgb(122, 255, 147)", "rgb(255,255,0)", "rgb(255, 200, 0)", "rgb(255, 200, 0)", "rgb(255, 145, 0)", "rgb(255, 0, 0)", "rgb(255, 145, 0)", "rgb(255, 0, 0)", "rgb(200, 0, 0)", "rgb(128, 0, 0)"];
-var mC =[];
+var mC = [];
 
 function popMap() {
   var mapStyle = [{
@@ -28,28 +28,23 @@ function popMap() {
   }];
   var options = { center: { lat: 20.5937, lng: 78.9629 }, zoom: 4, disableDefaultUI: true, zoomControl: true, styles: mapStyle };
   map = new google.maps.Map(document.getElementById('map'), options);
-
   bounds = new google.maps.LatLngBounds(new google.maps.LatLng(6, 65), new google.maps.LatLng(35.956, 97.35));
 
   google.maps.event.addListener(map, 'dragend', restrict);
   google.maps.event.addListener(map, 'drag', restrict);
   google.maps.event.addListener(map, 'resize', restrict);
-  google.maps.event.addListener(map, 'zoom_changed', zoom_restrict);
+  //google.maps.event.addListener(map, 'zoom_changed', zoom_restrict);
+
   var legend = document.getElementById('mmiScale');
-  // legend = legend.appendChild(document.createElement('div'));
-  // var tr= document.createElement('tr');
-  // tr.innerHTML = '<th>MMI</th><th>Colour</th>';
-  // legend.appendChild(tr);
-  // for (var colour in grad) {
-  //   var div = document.createElement('tr');
-  //   div.innerHTML = '<td>'+colour+'</td><td class=mmm'+colour+'>&nbsp;&nbsp;&nbsp;</td>';
-  //   legend.appendChild(div);
-  // }
   map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(legend);
+
+  // var script = document.createElement('script');
+  // script.src = 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime='+new Date() +'&callback=plotIso';
+  // document.body.appendChild(script);
 }
 
 function zoom_restrict() {
-  if(map.getZoom >= 3) return;
+  if (map.getZoom >= 3) return;
   map.setZoom(3);
 }
 
@@ -58,21 +53,23 @@ function restrict() {
   if (bounds.equals(map.getBounds())) return;
   map.fitBounds(bounds);
 }
-function recentEq(response) {
 
+function recentEq(response) {
   var rEqList = document.getElementById('recent-eq');
   rEqList = rEqList.appendChild(document.createElement("ul"));
   rEqList.className += "list-group";
   for (var i in response.features) {
-    var eq = document.createElement("li");
-    eq.className += "list-group-item";
-    var eqEvent = document.createElement("button");
-    eqEvent.className += "list-group-item list-group-item-action";
-    eqEvent.setAttribute("onclick", "loadEqEvent(\"" + response.features[i].properties.detail + "\")");
-    var title = document.createTextNode(response.features[i].properties.title);
-    eqEvent.appendChild(title);
-    eq.appendChild(eqEvent);
-    rEqList.appendChild(eq);
+    if (response.features[i].properties.mag > 5 || true ){
+      var eq = document.createElement("li");
+      eq.className += "list-group-item";
+      var eqEvent = document.createElement("button");
+      eqEvent.className += "list-group-item list-group-item-action";
+      eqEvent.setAttribute("onclick", "loadEqEvent(\"" + response.features[i].properties.detail + "\")");
+      var title = document.createTextNode(response.features[i].properties.title);
+      eqEvent.appendChild(title);
+      eq.appendChild(eqEvent);
+      rEqList.appendChild(eq);
+    }
   }
 }
 
@@ -93,7 +90,7 @@ function loadPoint(response) {
     center: { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] },
     radius: response.properties.mag * 30000
   });
-  // map.data.addGeoJson(response);
+
   var mag = response.properties.mag;
   map.data.addListener("mouseover", function (event) {
     var place = event.feature.getProperty("place").toString();
@@ -115,49 +112,49 @@ function loadPoint(response) {
 
 function conCircle(center, MMI, rad) {
   var iW;
-  for (var k in mC){
-    mC[k].setMap(null);
+  for (var h in mC) {
+    mC[h].setMap(null);
   }
+
   for (var k in rad) {
     mC.push(new google.maps.Circle({
       center: center,
       fillColor: grad[MMI[k] - 1],
       fillOpacity: 0.5,
-      strokeWeight: 0,
+      strokeWeight: 1,
+      strokeColor: grad[MMI[k] - 1],
       radius: rad[k] * 1000,
       map: map,
       zIndex: rad.length - k
     }));
-    //   google.maps.event.addListener(mC, "mouseover", info);
-    //   google.maps.event.addListener(mC, "mouseout", function (e) {
-    //     iW.close();
-    //   });
-    //   function info(e) {
-    //       iW = new google.maps.InfoWindow({
-    //       content: "MMI: " + (grad.findIndex(x => x === mC.fillColor)+1),
-    //       maxWidth: 300,
-    //       position: e.latLng
-    //     });
-    //     iW.open(map);
-    //   }
-    // }
-    map.panTo(center);
-    //map.fitBounds(mC[mC.length -1].getBounds());
+
+    google.maps.event.addListener(mC[k], "mouseover", function (e) {
+      iW = new google.maps.InfoWindow({
+        content: "MMI: " + (grad.findIndex(x => x === mC.fillColor) + 1),
+        maxWidth: 300,
+        position: e.latLng
+      });
+      iW.open(map);
+    });
+    google.maps.event.addListener(mC[k], "mouseout", function (e) {
+      iW.close();
+    });
   }
+  map.panTo(center);
+  map.fitBounds(mC[mC.length - 1].getBounds());
 }
 
-  // function intensity() { center: { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] }, var mag1 = response.properties.mag; var focal = response.origin.depth;  } }
-
 function plotIso(response) {
-  var MMI =[], rad =[];
-  var center= { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] };
-  var mag1 = response.properties.mag; 
+  var MMI = [], rad = [];
+  var center = { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] };
+  var mag1 = response.properties.mag;
   var focal = parseFloat(response.properties.products.origin[0].properties.depth);
-  var ei = Math.round(1.5 * mag1 - 3.5 * Math.log10(focal) + 3); 
-  for (var r = 5; r <= 100; r = r + 5) { 
-    var i = ei - (3.6) * Math.log10(Math.sqrt(1 + (r / focal) ^ 2));
+  var ei = 1.5 * mag1 - 3.5 * Math.log10(focal) + 3;
+  for (var r = 5; r <= 50; r = r + 5) {
+    var i = ei - (3.6) * Math.log10(Math.sqrt(1 + ((r / focal) ^ 2)));
     rad.push(r);
-    MMI.push(Math.floor(i));
+    MMI.push(Math.round(i));
   }
-  conCircle(center,MMI,rad);
+
+  conCircle(center, MMI, rad);
 }
