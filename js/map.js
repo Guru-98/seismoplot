f7ffd51aed06fe2fdc46b40ed4a4f5660cc02662
Guru Prasath G <theemailidofthemaster@gmail.com>
2017-04-01 19:40:48 +0530

    added MMIScale

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 var map;
 var bounds;
 var circle;
+var grad = ["rgb(255, 255, 255)", "rgb(191, 204, 255)", "rgb(160, 230, 255)", "rgb(128, 255, 255)", "rgb(122, 255, 147)", "rgb(255,255,0)", "rgb(255, 200, 0)", "rgb(255, 200, 0)", "rgb(255, 145, 0)", "rgb(255, 0, 0)", "rgb(255, 145, 0)", "rgb(255, 0, 0)", "rgb(200, 0, 0)", "rgb(128, 0, 0)"];
 
 function popMap() {
   var mapStyle = [{
@@ -32,9 +33,17 @@ function popMap() {
   google.maps.event.addListener(map, 'dragend', restrict);
   google.maps.event.addListener(map, 'drag', restrict);
   google.maps.event.addListener(map, 'resize', restrict);
-  var MMI = [7, 6, 6, 6, 5, 5, 5, 5, 5, 4];
-  var rad = [5,10,15,20,25,30,35,40,45,50];
-  conCircle(MMI,rad);
+  var legend = document.getElementById('mmiScale');
+  // legend = legend.appendChild(document.createElement('div'));
+  // var tr= document.createElement('tr');
+  // tr.innerHTML = '<th>MMI</th><th>Colour</th>';
+  // legend.appendChild(tr);
+  // for (var colour in grad) {
+  //   var div = document.createElement('tr');
+  //   div.innerHTML = '<td>'+colour+'</td><td class=mmm'+colour+'>&nbsp;&nbsp;&nbsp;</td>';
+  //   legend.appendChild(div);
+  // }
+  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(legend);
 }
 
 //TODO redefine restriction
@@ -42,8 +51,8 @@ function restrict() {
   if (bounds.equals(map.getBounds())) return;
   map.fitBounds(bounds);
 }
-
 function recentEq(response) {
+
   var rEqList = document.getElementById('recent-eq');
   rEqList = rEqList.appendChild(document.createElement("ul"));
   rEqList.className += "list-group";
@@ -62,7 +71,7 @@ function recentEq(response) {
 
 function loadEqEvent(url) {
   var script = document.createElement('script');
-  script.src = url + "&callback=loadPoint";
+  script.src = url + "&callback=plotIso";
   document.body.appendChild(script);
 }
 
@@ -77,14 +86,14 @@ function loadPoint(response) {
     center: { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] },
     radius: response.properties.mag * 30000
   });
-  map.data.addGeoJson(response);
+  // map.data.addGeoJson(response);
   var mag = response.properties.mag;
   map.data.addListener("mouseover", function (event) {
     var place = event.feature.getProperty("place").toString();
     var time = event.feature.getProperty("time").toString();
     var url = event.feature.getProperty("url").toString();
     var mag = event.feature.getProperty("mag").toString();
-    var info = "<b>Place: " + place + "</b><li>Time: " + time + "</li><li>url: " + url + "</li><li>Mag: " + mag + "</li>";
+    var info = "<b>Place: " + place + "</b><li>Time: " + time + "</li><li>Mag: " + mag + "</li><li><a herf=\"" + url + "\">More Info</li>";
     iW = new google.maps.InfoWindow({ content: info, maxWidth: 300, position: event.latLng, pixelOffset: new google.maps.Size(0, -25) });
     iW.open(map);
   });
@@ -97,33 +106,46 @@ function loadPoint(response) {
   });
 }
 
-function conCircle(MMI,rad) {
-  var mC,iW;
-  var grad = [rgb(255, 255, 255), rgb(191, 204, 255), rgb(160, 230, 255), rgb(128, 255, 255), rgb(122, 255, 147), rgb("255,255,0"), rgb(255, 200, 0), rgb(255, 200, 0), rgb(255, 145, 0), rgb(255, 0, 0), rgb(255, 145, 0), rgb(255, 0, 0), rgb(200, 0, 0), rgb(128, 0, 0)];
+function conCircle(center, MMI, rad) {
+  var mC, iW;
   for (var k in rad) {
     mC = new google.maps.Circle({
-      center: { lat: 20.5937, lng: 78.9629 },
-      fillColor: grad[MMI[k]-1],
+      center: center,
+      fillColor: grad[MMI[k] - 1],
       fillOpacity: 0.5,
       strokeWeight: 0,
-      radius: rad[k] * 10000,
+      radius: rad[k] * 1000,
       map: map,
       zIndex: rad.length - k
     });
-    google.maps.event.addListener(mC, "mouseover", function (e) {
-        iW = new google.maps.InfoWindow({
-        content: "MMI: " + MMI[k],
-        maxWidth: 300,
-        position: e.latLng
-      });
-      iW.open(map);
-    });
-    google.maps.event.addListener(mC, "mouseout", function (e) {
-      iW.close();
-    });
+    //   google.maps.event.addListener(mC, "mouseover", info);
+    //   google.maps.event.addListener(mC, "mouseout", function (e) {
+    //     iW.close();
+    //   });
+    //   function info(e) {
+    //       iW = new google.maps.InfoWindow({
+    //       content: "MMI: " + (grad.findIndex(x => x === mC.fillColor)+1),
+    //       maxWidth: 300,
+    //       position: e.latLng
+    //     });
+    //     iW.open(map);
+    //   }
+    // }
   }
 }
 
-function rgb(r, g, b){
-  return "rgb("+r+","+g+","+b+")";
+  // function intensity() { center: { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] }, var mag1 = response.properties.mag; var focal = response.origin.depth;  } }
+
+function plotIso(response) {
+  var MMI =[], rad =[];
+  var center= { lat: response.geometry.coordinates[1], lng: response.geometry.coordinates[0] };
+  var mag1 = response.properties.mag; 
+  var focal = parseFloat(response.properties.products.origin[0].properties.depth);
+  var ei = 1.5 * mag1 - 3.5 * Math.log10(focal) + 3; 
+  for (var r = 5; r <= 100; r = r + 5) { 
+    var i = ei - (3.6) * Math.log10(Math.sqrt(1 + (r / focal) ^ 2));
+    rad.push(r);
+    MMI.push(Math.floor(i));
+  }
+  conCircle(center,MMI,rad);
 }
